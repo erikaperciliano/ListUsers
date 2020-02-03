ede35b0540cd2a4a07537f23a719b3e6825cb519
Erika Perciliano <erikaperciliano.developer@gmail.com>
2020-02-03 17:30:46 -0300

    add projeto

---
+++ README.md
@@ -0,0 +1,20 @@
+# Lista de Usuários
+
+Com base no endpoint: http://jsonplaceholder.typicode.com/users, foi desenvolvido uma interface que lista através de cards os dados dos usuários como:
+
+  - Nome
+  - Username
+  - Email
+  - Telefone
+  - Website
+  - Endereço
+
+Além de exibir um elemento de filtrar os usuários que utilizem e-mail com o final do host:
+- .biz
+- .tv, 
+- .net
+- .org
+- .ca
+- .info
+- .me
+- .io
---
+++ js/requisicao.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+    var nome =  $('#nome').val();
+    var urlApi = "http://jsonplaceholder.typicode.com/users";
+
+    $.ajax({
+        url: urlApi,
+        type:"get",
+        dataType:"json",
+        success:function(conteudo){
+
+            for(let i = 0; i <= conteudo.length; i++){
+                
+                console.log(conteudo[i]);
+                $('#nome').append(i + ") " + conteudo[i].name + "<br>");
+                $('#userName').append(i + ") " + conteudo[i].username + "<br>");     
+                $('#email').append( "<tr> <td>"+ conteudo[i].email + " </tr> </td>");
+                $("table#email tr").addClass("linhas");
+                $('#celular').append(i + ") " + conteudo[i].phone + "<br>");
+                $('#webSite').append(i + ") " + conteudo[i].website + "<br>");
+                $('#endereco').append(i + ") Rua: " + conteudo[i].address.city + " / Rua: " + conteudo[i].address.street +
+                "/ " + conteudo[i].address.suite  + "<br>" + " Cep: " + conteudo[i].address.zipcode + "<br>" );
+            }
+
+        },
+        error:function(erro){
+            console.log(erro);
+        }
+    })
+
+
+  //busca
+  function busca(value,targetSelector){
+    $(targetSelector).show();
+    $(targetSelector+':not(:contains("'+ value +'"))').hide();
+}
+    $('#search').keyup(function () {
+    busca($(this).val(), '.linhas');
+    })
+});
